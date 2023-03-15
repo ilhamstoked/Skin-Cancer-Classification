fb066a9001c9786efc9ed9c04c6919da54ebec9c
Nanda Ilham Harahap <69181679+ilhamstoked@users.noreply.github.com>
2023-03-16 00:35:49 +0700

    updated ping

--- app1.py
+++ app1.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 from tcp_latency import measure_latency
 
 # Measure Latency
-latency = measure_latency(host='192.168.18.6', port=8501)
+latency = measure_latency(host='35.201.127.49', port=443)
 #35.201.127.49:443
+#192.168.18.6:8501
 
 # Load TensorFlow Lite model
 interpreter = tf.lite.Interpreter(model_path="InceptionResNetV2Skripsi.tflite")
